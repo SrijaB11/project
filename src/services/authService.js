732ffbbdcfc5bc53201cdf46d9068a9b8b056838
SrijaB11 <boddusrija11@gmail.com>
2026-03-01 16:34:28 +0530

    third

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://student-project-portal-backend-1.onrender.com",
 });
 
 export const registerUser = (data) => API.post("/auth/register", data);
